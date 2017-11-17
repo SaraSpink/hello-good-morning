eed3a71a35228314f94531c7120948497dbfedc6
Sara Spink <spinkbot@gmail.com>
2017-11-17 10:28:11 -0800

    pass unit tests for product and review database associations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 describe Product do
+  it { should have_many :reviews }
+  
   it { should validate_presence_of :name }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 describe Review do
+  it { should belong_to :product }
+
   it { should validate_presence_of :author }
 end
