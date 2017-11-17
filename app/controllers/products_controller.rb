fc884f1c8c4d6d71a72f770ceaf429b812fb2682
Sara Spink <spinkbot@gmail.com>
2017-11-17 14:02:30 -0800

    add update function for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,4 +11,18 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :cost, :country, :image)
+  end
 end
