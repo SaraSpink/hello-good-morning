ba4da9c2b792cfb0ab10f005c88a8861c0988c42
Sara Spink <spinkbot@gmail.com>
2017-11-17 17:48:34 -0800

    work on routing for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,15 @@ class ReviewsController < ApplicationController
   def index
     @reviews = Review.all
   end
+
+  def show
+    @review = Review.find(params[:id])
+    @product = @review.product
+
+  end
+
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+  end
 end
