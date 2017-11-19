d4d4e168dc62747e4032e85bccd3f36f692f1612
Sara Spink <spinkbot@gmail.com>
2017-11-18 18:35:35 -0700

    add review capabilities

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,4 +13,20 @@ class ReviewsController < ApplicationController
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new
   end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "Review Successfully added"
+      redirect_to product_path(@review.product)
+    else
+      render :new
+    end
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating)
+  end
 end
