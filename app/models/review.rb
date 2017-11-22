847231f929b7b470e6ac7ee1e91fb74fb7a6054c
Sara Spink <spinkbot@gmail.com>
2017-11-22 11:48:28 -0800

    create more validations for reviews, including requiring rating to be a number between 1 and 5, and also it will display rating on product show page

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :product
 
   validates :author, :presence => true
+  validates :content_body, :presence => true, :length => { :minimum => 50, :maximum => 250 }
+  validates :rating,  numericality: true, :presence => true, :inclusion => 1..5 
+  validates :product_id, :presence => true
 end
